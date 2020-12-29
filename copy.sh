6423746b318929b2a66b54a652e97240a30522ae
Subhaditya Nath <sn03.general@gmail.com>
2020-12-29 23:19:02 +0530

    More files! Added copy.sh script to copy

---
+++ copy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+cp /etc/profile.d/polybar.sh -t ~/dotfiles-artix-s6/root/etc/profile.d/ -rv
+cp ~/.config/sx -t ~/dotfiles-artix-s6/root/home/subhaditya/.config -rv
+cp ~/.local/bin  ~/dotfiles-artix-s6/root/home/subhaditya/.local/bin -rv
