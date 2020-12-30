538b91715339e64e5032ac0b8ddb145a17273e0e
Subhaditya Nath <sn03.general@gmail.com>
2020-12-30 10:28:58 +0530

    Added all_installed_programs

--- copy.sh
+++ copy.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
-cp /etc/profile.d/polybar.sh -t ~/dotfiles-artix-s6/root/etc/profile.d/ -rv
-cp ~/.config/sx -t ~/dotfiles-artix-s6/root/home/subhaditya/.config -rv
-cp ~/.local/bin  ~/dotfiles-artix-s6/root/home/subhaditya/.local/bin -rv
+ROOT_DIR=$(
+  cd "$(dirname "$0")"
+  pwd -P
+)
+cd "$ROOT_DIR"
+
+cp /etc/profile.d/polybar.sh -t ./root/etc/profile.d/ -rv
+cp ~/.config/sx -t ./root/home/subhaditya/.config -rv
+cp ~/.local/bin    ./root/home/subhaditya/.local/ -rv
+pacman -Qe | cut -d' ' -f1 > ./all_installed_programs
--- notes.md
+++ notes.md
@@ -1,3 +1,6 @@
 # Sleep / Suspend
-[This.](https://www.kernel.org/doc/Documentation/power/states.txt)
+[This.](https://www.kernel.org/doc/Documentation/power/states.txt)  
 And [this](https://wiki.archlinux.org/index.php/Power_management#Power_management_with_systemd) too. (elogind provides this functionality)
+
+# Automount
+Install [`udiskie`](https://github.com/coldfix/udiskie) from the repo
