33c98bc069f92f2a8eb5088222b62529111722e5
Subhaditya Nath <sn03.general@gmail.com>
2021-01-20 01:06:53 +0530

    Added kitty config too

--- copy.sh
+++ copy.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/sh
 ROOT_DIR=$(
-  cd "$(dirname "$0")"
-  pwd -P
+	cd "$(dirname "$0")"
+	pwd -P
 )
 cd "$ROOT_DIR"
 
-cp /etc/profile.d/polybar.sh -t ./root/etc/profile.d/ -rv
-cp /etc/modprobe.d/alsa.conf -t ./root/etc/modprobe.d -rv
-cp ~/.config/sx -t ./root/home/subhaditya/.config -rv
-cp ~/.local/bin    ./root/home/subhaditya/.local/ -rv
-cp ~/.zshrc        ./root/home/subhaditya/.zshrc
-cp ~/.p10k.zsh     ./root/home/subhaditya/.p10k.zsh
-cp ~/.config/nvim/init.vim -t ./root/home/subhaditya/.config/nvim -rv
+cp /etc/profile.d/polybar.sh	-t	./root/etc/profile.d/								-rv
+cp /etc/modprobe.d/alsa.conf	-t	./root/etc/modprobe.d 							-rv
+cp ~/.config/sx								-t	./root/home/subhaditya/.config			-rv
+cp ~/.config/kitty						-t	./root/home/subhaditya/.config			-rv
+cp ~/.local/bin										./root/home/subhaditya/.local/			-rv
+cp ~/.zshrc												./root/home/subhaditya/.zshrc
+cp ~/.p10k.zsh										./root/home/subhaditya/.p10k.zsh
+cp ~/.config/nvim/init.vim		-t	./root/home/subhaditya/.config/nvim -rv
 
 pacman -Qe | cut -d' ' -f1 > ./all_installed_programs
-# vim: et
+
+# vim: noet ts=2 sts=2
