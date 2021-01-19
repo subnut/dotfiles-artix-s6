1a5773d380f27e223cb3649f783d9c79ebdcba2d
Subhaditya Nath <sn03.general@gmail.com>
2021-01-20 01:29:09 +0530

    Added bspwmrc sxhkdrc

--- copy.sh
+++ copy.sh
@@ -5,15 +5,17 @@ ROOT_DIR=$(
 )
 cd "$ROOT_DIR"
 
-cp /etc/profile.d/polybar.sh	-t	./root/etc/profile.d/								-rv
-cp /etc/modprobe.d/alsa.conf	-t	./root/etc/modprobe.d 							-rv
-cp ~/.config/sx								-t	./root/home/subhaditya/.config			-rv
-cp ~/.config/kitty						-t	./root/home/subhaditya/.config			-rv
-cp ~/.local/bin										./root/home/subhaditya/.local/			-rv
-cp ~/.zshrc												./root/home/subhaditya/.zshrc
-cp ~/.p10k.zsh										./root/home/subhaditya/.p10k.zsh
-cp ~/.config/nvim/init.vim		-t	./root/home/subhaditya/.config/nvim -rv
+cp /etc/profile.d/polybar.sh	-t	./root/etc/profile.d/			-rv
+cp /etc/modprobe.d/alsa.conf	-t	./root/etc/modprobe.d 			-rv
+cp ~/.config/sx			-t	./root/home/subhaditya/.config		-rv
+cp ~/.config/kitty		-t	./root/home/subhaditya/.config		-rv
+cp ~/.config/bspwm		-t	./root/home/subhaditya/.config		-rv
+cp ~/.config/sxhkd		-t	./root/home/subhaditya/.config		-rv
+cp ~/.local/bin				./root/home/subhaditya/.local/		-rv
+cp ~/.zshrc				./root/home/subhaditya/.zshrc
+cp ~/.p10k.zsh				./root/home/subhaditya/.p10k.zsh
+cp ~/.config/nvim/init.vim	-t	./root/home/subhaditya/.config/nvim	-rv
 
 pacman -Qe | cut -d' ' -f1 > ./all_installed_programs
 
-# vim: noet ts=2 sts=2
+# vim: noet ts=8 sts=8
